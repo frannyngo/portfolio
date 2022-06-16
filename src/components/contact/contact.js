7664219a987490696004795c0c2c5a2b90899076
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-06-15 21:41:36 -0700

    contact_form_design

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,6 +1,13 @@
 import React, { useRef } from "react";
 import { Container, H5, H2 } from "../../global_styles/theme";
-import { Contact_Container } from "./contact_styles";
+import {
+  Contact_Container,
+  Form,
+  Send_Button,
+  Label_Input,
+  Text_Input,
+  Input_Style,
+} from "./contact_styles";
 import { MdOutlineEmail } from "react-icons/md";
 import emailjs from "@emailjs/browser";
 
@@ -32,15 +39,15 @@ export default function Contact() {
       <H5 style={{ color: "rgb(255, 255, 255, 0.6" }}>Get In Touch</H5>
       <H2>Contact Me</H2>
       <Contact_Container>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="user_name" />
-          <label>Email</label>
-          <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
-        </form>
+        <Form ref={form} onSubmit={sendEmail}>
+          <Label_Input>Name</Label_Input>
+          <Input_Style type="text" name="user_name" />
+          <Label_Input>Email</Label_Input>
+          <Input_Style type="email" name="user_email" />
+          <Label_Input>Message</Label_Input>
+          <Text_Input name="message" />
+          <Send_Button type="submit" value="Send" />
+        </Form>
       </Contact_Container>
     </Container>
   );
--- src/components/contact/contact_styles.js
+++ src/components/contact/contact_styles.js
@@ -13,3 +13,81 @@ export const Contact_Container = styled.div`
     flex-direction: column;
   }
 `;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  border-radius: 10px;
+  border: 2px solid #d3ae36;
+  width: 70%;
+  padding: 50px;
+  flex-direction: column;
+  max-width: 600px;
+  box-shadow: 0px 10px 15px rgb(0, 0, 0, 0.14);
+
+  @media (max-width: 680px) {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 80%;
+    height: 500px;
+    padding: 20px;
+  }
+`;
+
+export const Send_Button = styled.input`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #d3ae36;
+  border: 2px solid #d3ae36;
+  background: #fffdfa;
+  border-radius: 10px;
+  padding: 20px;
+  cursor: pointer;
+  text-decoration: none;
+  width: 125px;
+  margin-top: 10px;
+
+  &:hover {
+    background: #faf6f0;
+  }
+
+  @media (max-width: 450px) {
+    width: 93%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Label_Input = styled.label`
+  color: #a6a6a6;
+  font-size: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+
+  @media (max-width: 450px) {
+    margin-right: 5px;
+  }
+`;
+
+export const Text_Input = styled.textarea`
+  color: #a6a6a6;
+  font-size: 20px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  border: 2px solid #d3ae36;
+  height: 100px;
+  width: 90%;
+`;
+
+export const Input_Style = styled.input`
+  color: #a6a6a6;
+  font-size: 20px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  border: 2px solid #d3ae36;
+  width: 90%;
+  height: 40px;
+`;
