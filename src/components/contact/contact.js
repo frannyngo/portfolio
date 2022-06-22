0e7c44eb9076edec293f37ffc6a998963c69cd8f
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-06-21 21:09:08 -0700

    Revert "contact_image"
    This reverts commit 35d7bdfbf173dba31656f46a08f62da9a5ea67c2.
    
    revert changes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,19 +7,12 @@ import {
   Label_Input,
   Text_Input,
   Input_Style,
-  Contact_Container_Invisible,
-  Image_Container,
-  Contact_Content_Container,
-  Image_Container_Invisible,
 } from "./contact_styles";
-import { BiMailSend } from "react-icons/bi";
+import { MdOutlineEmail } from "react-icons/md";
 import emailjs from "@emailjs/browser";
-import { useInView } from "react-intersection-observer";
 
 export default function Contact() {
   const form = useRef();
-  const { ref: ref, inView: is_visible_Form } = useInView();
-  const { ref: ref_image, inView: is_visible_image } = useInView();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -33,8 +26,7 @@ export default function Contact() {
       )
       .then(
         (result) => {
-          console.log(result);
-          e.target.reset();
+          console.log(result.text);
         },
         (error) => {
           console.log(error.text);
@@ -46,43 +38,17 @@ export default function Contact() {
     <Container id="contact">
       <H5 style={{ color: "rgb(255, 255, 255, 0.6" }}>Get In Touch</H5>
       <H2>Contact Me</H2>
-
-      <Contact_Content_Container>
-        {is_visible_image ? (
-          <Image_Container ref={ref_image}>
-            <BiMailSend style={{ height: 400, width: 400, color: "#d3ae36" }} />
-          </Image_Container>
-        ) : (
-          <Image_Container_Invisible ref={ref_image}>
-            <BiMailSend style={{ height: 400, width: 400, color: "#d3ae36" }} />
-          </Image_Container_Invisible>
-        )}
-        {is_visible_Form ? (
-          <Contact_Container ref={ref}>
-            <Form ref={form} onSubmit={sendEmail}>
-              <Label_Input>Name</Label_Input>
-              <Input_Style type="text" name="user_name" />
-              <Label_Input>Email</Label_Input>
-              <Input_Style type="email" name="user_email" />
-              <Label_Input>Message</Label_Input>
-              <Text_Input name="message" />
-              <Send_Button type="submit" value="Send" />
-            </Form>
-          </Contact_Container>
-        ) : (
-          <Contact_Container_Invisible ref={ref}>
-            <Form ref={form} onSubmit={sendEmail}>
-              <Label_Input>Name</Label_Input>
-              <Input_Style type="text" name="user_name" />
-              <Label_Input>Email</Label_Input>
-              <Input_Style type="email" name="user_email" />
-              <Label_Input>Message</Label_Input>
-              <Text_Input name="message" />
-              <Send_Button type="submit" value="Send" />
-            </Form>
-          </Contact_Container_Invisible>
-        )}
-      </Contact_Content_Container>
+      <Contact_Container>
+        <Form ref={form} onSubmit={sendEmail}>
+          <Label_Input>Name</Label_Input>
+          <Input_Style type="text" name="user_name" />
+          <Label_Input>Email</Label_Input>
+          <Input_Style type="email" name="user_email" />
+          <Label_Input>Message</Label_Input>
+          <Text_Input name="message" />
+          <Send_Button type="submit" value="Send" />
+        </Form>
+      </Contact_Container>
     </Container>
   );
 }
